7da9e930b24dd9783ae840bf4a55c592f4f36619
Rwitesh Bera <rwiteshbera@gmail.com>
2024-12-19 00:16:00 +0530

    Update clickhouse.go

--- backend/database/clickhouse.go
+++ backend/database/clickhouse.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"fmt"
 	"log"
@@ -18,7 +19,7 @@ func ConnectClickhouse(config ClickhouseConfig) *sql.DB {
 	DB := clickhouse.OpenDB(&clickhouse.Options{
 		Addr:     []string{config.URL},
 		Protocol: clickhouse.Native,
-		// TLS:      &tls.Config{},
+		TLS:      &tls.Config{},
 		Auth: clickhouse.Auth{
 			Username: config.Username,
 			Password: config.Password,
